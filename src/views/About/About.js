d7042ddd280c977831a5df730e4386369f371f96
pmarinelli18 <pmarinelli18@gmail.com>
2021-10-05 23:19:50 -0400

    added new resume

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -80,7 +80,7 @@ const About = () => {
 								</Link>
 							</Box>
 							<Box alignSelf="center">
-								<a href={"resume20-11-23.pdf"} target="_blank">
+								<a href={"resume21-10-5.pdf"} target="_blank">
 									<Button label="Resume" />
 								</a>
 							</Box>
